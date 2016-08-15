c1b3f330f3c1f52347e9cf29fe4d67f038a52499
cat <cat@cathole>
2016-08-15 21:31:26 +0400

    добавил вывод имени компьютера

--- td/src/main/java/epk/sam/uit/td/Start.java
+++ td/src/main/java/epk/sam/uit/td/Start.java
@@ -2,8 +2,9 @@ package epk.sam.uit.td;
 
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.printf("стартовал %s%n" , Thread.currentThread().getName());
+		System.out.printf("эту строку я добавил дома. Имя компьютера:  %s%n" , java.net.InetAddress.getLocalHost().getHostName() );
 	}
 
 }
